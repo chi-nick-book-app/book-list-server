48f63bd4c94bebeb23b065ac4ff6654c63d75482
StarGale <zhenchix@hotmail.com>
2018-06-18 17:28:32 -0700

    switch role

--- server.js
+++ server.js
@@ -12,15 +12,34 @@ const PORT = process.env.PORT;
 
 //Database Setup
 
+//For mac
+// const conString = 'postgres://localhost:5432/books_app';
+
+//For linux
+// const conString = "postgresql://postgres:1234@localhost:5432/books_app";
+// const conString = process.env.DATABASE_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.log(err));
 
 //Application Middleweare
 app.use(cors());
 
 //API Endpoints
-app.get('/api/v1/test', (req, res) => {
-    console.log('OMG I am in contact with the srver !!!!!')
-    res.send('OMG I am in contact with the server !!!!!');
+app.get('/api/v1/books', (req, res) => {
+
+    console.log('OMG I am handling a GET request by a client !!');
+    let SQL = `SELECT *  FROM books;`;
+    client.query(SQL)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+    // res.send('OMG I am in contact with the server !!!!!');
+
+
+
 });
+
 
 //SETUP 404
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
