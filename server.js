e40e69bfb1eaa926bd339ae141de5dc888a38b2f
StarGale <zhenchix@hotmail.com>
2018-06-21 19:06:06 -0700

    server-js update

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ const PORT = process.env.PORT;
 
 //For linux
 // const conString = "postgresql://chi:1234@localhost:5432/books_app";
-// const conString = process.env.DATABASE_URL;
+const conString = process.env.DATABASE_URL;
+const client = new pg.Client(conString);
 
-const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.log(err));
 
@@ -31,6 +32,7 @@ app.use(cors());
 
 //app.post create book
 app.post('api/v1/books', (req, res) => {
+    console.log('test the form route')
     let SQL = `
 INSERT INTO books(title, author, isbn, image_url, description)
 VALUES($1, $2, $3, $4, $5);`;
@@ -43,16 +45,6 @@ VALUES($1, $2, $3, $4, $5);`;
     client.query(SQL, values)
         .then(() => res.send('insert complete'))
         .catch(console.error);
-});
-// app.get  pick one book
-app.get('/api/v1/books/:id', (req, res) => {
-    let SQL = `SELECT * FROM books WHERE book_id=$1;`;
-    let values = [req.params.id];
-    client.query(SQL, values)
-        .then(() => res.send('whatever,just pick one !'))
-        .catch(console.error);
-
-
 });
 
 //app.put   update books
@@ -77,7 +69,7 @@ app.put('/api/v1/books/:id', (req, res) => {
         .catch(console.error);
 
 
-    // app.post
+   
 
 });
 
@@ -89,6 +81,18 @@ app.delete('/api/v1/books/:id', (req, res) => {
     client.query(SQL, values)
         .then(() => res.send('Delete complete'))
         .catch(console.error);
+});
+
+// app.get  pick one book
+app.get('/api/v1/books/:id', (req, res) => {
+
+    let SQL = `SELECT * FROM books WHERE book_id=${req.params.id};`;
+    client.query(SQL)
+        .then(results => res.send(results.rows))
+        .catch(console.error);
+        console.log('whatever,just pick one !');
+
+
 });
 
 
@@ -103,11 +107,7 @@ app.get('/api/v1/books', (req, res) => {
         .catch(console.error);
     // res.send('OMG I am in contact with the server !!!!!');
 
-
-
 });
-
-
 
 
 //SETUP 404
